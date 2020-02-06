766f3094abd6c314f607e361a116595403685893
Chris Ohk <utilForever@gmail.com>
2020-02-06 23:27:36 +0900

    feat(impl-wordobj): Refactor method 'Add()' and 'Remove()'

--- Includes/baba-is-auto/Rules/RuleManager.hpp
+++ Includes/baba-is-auto/Rules/RuleManager.hpp
@@ -16,28 +16,12 @@ class RuleManager
 {
  public:
     //! Adds a rule.
-    //! \param noun A noun word.
-    //! \param op An operator word.
-    //! \param property A property word.
-    void Add(Noun noun, Operator op, Property property);
-
-    //! Adds a rule.
-    //! \param noun1 A first noun word.
-    //! \param op An operator word.
-    //! \param noun1 A second noun word.
-    void Add(Noun noun1, Operator op, Noun noun2);
+    //! \param rule The rule.
+    void Add(Rule rule);
 
     //! Removes a rule.
-    //! \param noun A noun word.
-    //! \param op An operator word.
-    //! \param property A property word.
-    void Remove(Noun noun, Operator op, Property property);
-
-    //! Removes a rule.
-    //! \param noun1 A first noun word.
-    //! \param op An operator word.
-    //! \param noun1 A second noun word.
-    void Remove(Noun noun1, Operator op, Noun noun2);
+    //! \param rule The rule.
+    void Remove(Rule rule);
 
     //! Gets the number of rules.
     //! \return The number of rules.
--- Sources/baba-is-auto/Rules/RuleManager.cpp
+++ Sources/baba-is-auto/Rules/RuleManager.cpp
@@ -4,55 +4,13 @@
 
 namespace baba_is_auto
 {
-void RuleManager::Add(Noun noun, Operator op, Property property)
+void RuleManager::Add(Rule rule)
 {
-    Rule rule{};
-
-    rule.type = RuleType::NOUN_VERB_PROPERTY;
-    rule.noun1 = noun;
-    rule.op = op;
-    rule.property = property;
-
     m_rules.emplace_back(rule);
 }
 
-void RuleManager::Add(Noun noun1, Operator op, Noun noun2)
+void RuleManager::Remove(Rule rule)
 {
-    Rule rule{};
-
-    rule.type = RuleType::NOUN_VERB_NOUN;
-    rule.noun1 = noun1;
-    rule.op = op;
-    rule.noun2 = noun2;
-
-    m_rules.emplace_back(rule);
-}
-
-void RuleManager::Remove(Noun noun, Operator op, Property property)
-{
-    Rule rule{};
-
-    rule.type = RuleType::NOUN_VERB_PROPERTY;
-    rule.noun1 = noun;
-    rule.op = op;
-    rule.property = property;
-
-    const auto iter = std::find(m_rules.begin(), m_rules.end(), rule);
-    if (iter != m_rules.end())
-    {
-        m_rules.erase(iter);
-    }
-}
-
-void RuleManager::Remove(Noun noun1, Operator op, Noun noun2)
-{
-    Rule rule{};
-
-    rule.type = RuleType::NOUN_VERB_NOUN;
-    rule.noun1 = noun1;
-    rule.op = op;
-    rule.noun2 = noun2;
-
     const auto iter = std::find(m_rules.begin(), m_rules.end(), rule);
     if (iter != m_rules.end())
     {
