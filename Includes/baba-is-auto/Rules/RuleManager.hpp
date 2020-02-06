833d35992c6ec670e5f386703908ba4e8e6b7acd
Chris Ohk <utilForever@gmail.com>
2020-02-06 14:11:14 +0900

    feat(impl-wordobj): Add method 'Add()'
    - Adds a rule

--- Includes/baba-is-auto/Rules/RuleManager.hpp
+++ Includes/baba-is-auto/Rules/RuleManager.hpp
@@ -1,6 +1,10 @@
 #ifndef BABA_IS_AUTO_RULE_MANAGER_HPP
 #define BABA_IS_AUTO_RULE_MANAGER_HPP
 
+#include <baba-is-auto/Rules/Rule.hpp>
+
+#include <vector>
+
 namespace baba_is_auto
 {
 //!
@@ -10,6 +14,21 @@ namespace baba_is_auto
 //!
 class RuleManager
 {
+ public:
+    //! Adds a rule.
+    //! \param noun A noun word.
+    //! \param op An operator word.
+    //! \param property A property word.
+    void Add(Noun noun, Operator op, Property property);
+
+    //! Adds a rule.
+    //! \param noun1 A first noun word.
+    //! \param op An operator word.
+    //! \param noun1 A second noun word.
+    void Add(Noun noun1, Operator op, Noun noun2);
+
+ private:
+    std::vector<Rule> m_rules;
 };
 }  // namespace baba_is_auto
 
--- Sources/baba-is-auto/Rules/RuleManager.cpp
+++ Sources/baba-is-auto/Rules/RuleManager.cpp
@@ -2,4 +2,27 @@
 
 namespace baba_is_auto
 {
+void RuleManager::Add(Noun noun, Operator op, Property property)
+{
+    Rule rule{};
+
+    rule.type = RuleType::NOUN_VERB_PROPERTY;
+    rule.noun1 = noun;
+    rule.op = op;
+    rule.property = property;
+
+    m_rules.emplace_back(rule);
+}
+
+void RuleManager::Add(Noun noun1, Operator op, Noun noun2)
+{
+    Rule rule{};
+
+    rule.type = RuleType::NOUN_VERB_NOUN;
+    rule.noun1 = noun1;
+    rule.op = op;
+    rule.noun2 = noun2;
+
+    m_rules.emplace_back(rule);
+}
 }  // namespace baba_is_auto
