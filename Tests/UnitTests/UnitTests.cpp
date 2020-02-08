ccfcdce7dfe4ff8cd167dc14514614c6ab77850d
Chris Ohk <utilForever@gmail.com>
2020-02-09 00:32:07 +0900

    test: Add unit test to check methods of class 'Map'

---
+++ Tests/UnitTests/UnitTests.cpp
@@ -0,0 +1,37 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#undef NEAR
+
+#include <baba-is-auto/Games/Map.hpp>
+#include <baba-is-auto/Rules/RuleManager.hpp>
+
+using namespace baba_is_auto;
+
+TEST_CASE("Map - Basic")
+{
+    Map map(5, 5);
+
+    map.Assign(3, 4, ObjectType::BABA);
+    CHECK(map.At(3, 3).GetType() == ObjectType::EMPTY);
+    CHECK(map.At(3, 4).GetType() == ObjectType::BABA);
+}
+
+TEST_CASE("RuleManager - Basic")
+{
+    RuleManager ruleManager;
+
+    const Rule rule1{ Object(ObjectType::BABA), Object(ObjectType::IS),
+                      Object(ObjectType::YOU) };
+    const Rule rule2{ Object(ObjectType::KEKE), Object(ObjectType::IS),
+                      Object(ObjectType::STOP) };
+
+    ruleManager.Add(rule1);
+    CHECK(ruleManager.GetNumRules() == 1);
+
+    ruleManager.Add(rule2);
+    CHECK(ruleManager.GetNumRules() == 2);
+
+    ruleManager.Remove(rule2);
+    CHECK(ruleManager.GetNumRules() == 1);
+}
