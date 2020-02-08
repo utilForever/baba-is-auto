7f46f4af756e943bc773d85e9f823dcba7793942
Chris Ohk <utilForever@gmail.com>
2020-02-09 00:25:51 +0900

    feat(impl-card): Delete old files

--- Includes/baba-is-auto/baba-is-auto.hpp
+++ Includes/baba-is-auto/baba-is-auto.hpp
@@ -8,14 +8,10 @@
 #define BABA_IS_AUTO_HPP
 
 #include <baba-is-auto/Enums/RuleEnums.hpp>
-#include <baba-is-auto/Enums/WordEnums.hpp>
 #include <baba-is-auto/Games/Game.hpp>
 #include <baba-is-auto/Games/Map.hpp>
 #include <baba-is-auto/Rules/Rule.hpp>
 #include <baba-is-auto/Rules/RuleManager.hpp>
-#include <baba-is-auto/Words/Noun.hpp>
-#include <baba-is-auto/Words/Operator.hpp>
-#include <baba-is-auto/Words/Property.hpp>
 #include <baba-is-auto/baba-is-auto.hpp>
 
 #endif  // BABA_IS_AUTO_HPP
