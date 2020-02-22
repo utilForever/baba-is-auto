8436982b03228a78ce4d38e9638c30773edaee53
Chris Ohk <utilForever@gmail.com>
2020-02-23 01:02:01 +0900

    feat(impl-game): Add code to initialize a list of objects

--- Sources/baba-is-auto/Games/Map.cpp
+++ Sources/baba-is-auto/Games/Map.cpp
@@ -8,6 +8,12 @@ Map::Map(std::size_t width, std::size_t height)
     : m_width(width), m_height(height)
 {
     m_objects.reserve(m_width * m_height);
+
+    for (std::size_t i = 0; i < m_width * m_height; ++i)
+    {
+        m_objects.emplace_back(
+            std::vector<ObjectType>{ ObjectType::ICON_EMPTY });
+    }
 }
 
 std::size_t Map::GetWidth() const
