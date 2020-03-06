28ba1f05af4db6bc2f7ebfa64e2d0ba4a58c49bb
Chris Ohk <utilForever@gmail.com>
2020-03-06 19:52:48 +0900

    feat(python): Add code to define default constructor

--- Extensions/BabaPython/Sources/Rules/RuleManager.cpp
+++ Extensions/BabaPython/Sources/Rules/RuleManager.cpp
@@ -8,6 +8,7 @@ using namespace baba_is_auto;
 void AddRuleManager(pybind11::module& m)
 {
     pybind11::class_<RuleManager>(m, "RuleManager")
+        .def(pybind11::init<>())
         .def("AddRule", &RuleManager::AddRule)
         .def("RemoveRule", &RuleManager::RemoveRule)
         .def("ClearRules", &RuleManager::ClearRules)
