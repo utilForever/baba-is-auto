af4e1f9d2db9c814620c50b167eb9833255fb32c
Chris Ohk <utilForever@gmail.com>
2020-03-06 20:08:48 +0900

    feat(python): Create function for 'Object'

---
+++ Extensions/BabaPython/Sources/Games/Object.cpp
@@ -0,0 +1,26 @@
+#include <Games/Object.hpp>
+#include <baba-is-auto/Games/Object.hpp>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+using namespace baba_is_auto;
+
+void AddObject(pybind11::module& m)
+{
+    pybind11::class_<Object>(m, "Object")
+        .def(pybind11::init<>())
+        .def(pybind11::init<std::vector<ObjectType>>())
+        .def("__eq__",
+             [](const Object& left, const Object& right) {
+                 return left.GetTypes() == right.GetTypes();
+             })
+        .def("Add", &Object::Add)
+        .def("Remove", &Object::Remove)
+        .def("GetTypes", &Object::GetTypes)
+        .def("HasType", &Object::HasType)
+        .def("HasTextType", &Object::HasTextType)
+        .def("HasNounType", &Object::HasNounType)
+        .def("HasVerbType", &Object::HasVerbType)
+        .def("HasPropertyType", &Object::HasPropertyType);
+}
