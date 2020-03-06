80b6c94be5419e8f8d171ed09843f178f738985a
Chris Ohk <utilForever@gmail.com>
2020-03-06 20:09:42 +0900

    feat(python): Add unit tests for Python API

---
+++ Tests/PythonTests/test_game.py
@@ -0,0 +1,51 @@
+"""
+Copyright (c) 2019 Chris Ohk, Hyojang Kim
+
+We are making my contributions/submissions to this project solely in our
+personal capacity and are not conveying any rights to any intellectual
+property of any third parties.
+"""
+
+import pyBaba
+
+def test_game_basic():
+    game = pyBaba.Game("Resources/Maps/BabaIsYou.txt")
+    assert game.GetMap().At(4, 1).HasType(pyBaba.ObjectType.ICON_BABA)
+    assert game.GetMap().At(4, 9).HasType(pyBaba.ObjectType.ICON_FLAG)
+    assert game.GetRuleManager().GetNumRules() == 4
+    assert game.GetPlayerIcon() == pyBaba.ObjectType.ICON_BABA
+    pos = game.GetMap().GetPositions(game.GetPlayerIcon())
+    assert len(pos) == 1
+    assert pos[0][0] == 4
+    assert pos[0][1] == 1
+    game.MovePlayer(pyBaba.Direction.UP)
+    assert game.GetMap().At(3, 1).HasType(pyBaba.ObjectType.ICON_BABA)
+    assert game.GetMap().At(4, 1).HasType(pyBaba.ObjectType.ICON_EMPTY)
+    game.MovePlayer(pyBaba.Direction.UP)
+    assert game.GetMap().At(3, 1).HasType(pyBaba.ObjectType.ICON_BABA)
+    assert game.GetMap().At(2, 1).HasType(pyBaba.ObjectType.ICON_WALL)
+    game.MovePlayer(pyBaba.Direction.RIGHT)
+    game.MovePlayer(pyBaba.Direction.RIGHT)
+    game.MovePlayer(pyBaba.Direction.RIGHT)
+    assert game.GetMap().At(3, 4).HasType(pyBaba.ObjectType.ICON_BABA)
+    assert game.GetMap().At(3, 3).HasType(pyBaba.ObjectType.ICON_EMPTY)
+    game.MovePlayer(pyBaba.Direction.RIGHT)
+    assert game.GetMap().At(3, 5).HasType(pyBaba.ObjectType.ICON_BABA)
+    assert game.GetMap().At(3, 6).HasType(pyBaba.ObjectType.ICON_ROCK)
+    assert game.GetMap().At(3, 4).HasType(pyBaba.ObjectType.ICON_EMPTY)
+    game.MovePlayer(pyBaba.Direction.RIGHT)
+    game.MovePlayer(pyBaba.Direction.DOWN)
+    assert game.GetPlayState() == pyBaba.PlayState.PLAYING
+    game.MovePlayer(pyBaba.Direction.RIGHT)
+    game.MovePlayer(pyBaba.Direction.RIGHT)
+    game.MovePlayer(pyBaba.Direction.RIGHT)
+    assert game.GetPlayState() == pyBaba.PlayState.WON
+
+def test_game_lost():
+    game = pyBaba.Game("Resources/Maps/SimpleMap.txt")
+    assert game.GetMap().At(2, 0).HasType(pyBaba.ObjectType.ICON_BABA)
+    assert game.GetRuleManager().GetNumRules() == 1
+    assert game.GetPlayState() == pyBaba.PlayState.PLAYING
+    game.MovePlayer(pyBaba.Direction.UP)
+    assert game.GetRuleManager().GetNumRules() == 0
+    assert game.GetPlayState() == pyBaba.PlayState.LOST
---
+++ Tests/PythonTests/test_map.py
@@ -0,0 +1,15 @@
+"""
+Copyright (c) 2019 Chris Ohk, Hyojang Kim
+
+We are making my contributions/submissions to this project solely in our
+personal capacity and are not conveying any rights to any intellectual
+property of any third parties.
+"""
+
+import pyBaba
+
+def test_map_basic():
+    map = pyBaba.Map(5, 5)
+    map.AddObject(3, 4, pyBaba.ObjectType.BABA)
+    assert map.At(3, 3).HasType(pyBaba.ObjectType.ICON_EMPTY) == True
+    assert map.At(3, 4).HasType(pyBaba.ObjectType.BABA) == True
