66737d7f70ebad40b02306656248448244f59f3b
Chris Ohk <utilforever@gmail.com>
2022-06-07 19:35:38 +0900

    fix: Replace 'SpriteLoader' with 'MapSprite'

--- Extensions/BabaGUI/sprites.py
+++ Extensions/BabaGUI/sprites.py
@@ -1,43 +1,18 @@
 import pygame
 import pyBaba
-from images import GIFImage
 
 
-class SpriteLoader:
-    def __init__(self):
-        self.icon_images = {pyBaba.ObjectType.ICON_BABA: 'BABA',
-                            pyBaba.ObjectType.ICON_FLAG: 'FLAG',
-                            pyBaba.ObjectType.ICON_WALL: 'WALL',
-                            pyBaba.ObjectType.ICON_ROCK: 'ROCK',
-                            pyBaba.ObjectType.ICON_TILE: 'TILE',
-                            pyBaba.ObjectType.ICON_WATER: 'WATER',
-                            pyBaba.ObjectType.ICON_GRASS: 'GRASS',
-                            pyBaba.ObjectType.ICON_LAVA: 'LAVA'}
+class MapSprite(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, is_icon):
+        if is_icon:
+            self.image = pygame.image.load('./sprites/icon/{}.gif'.format(image))
+        else:
+            self.image = pygame.image.load('./sprites/text/{}.gif'.format(image))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-        for i in self.icon_images:
-            self.icon_images[i] = GIFImage(
-                './sprites/icon/{}.gif'.format(self.icon_images[i]))
-            self.icon_images[i].scale(1.0)
-
-        self.text_images = {pyBaba.ObjectType.BABA: 'BABA',
-                            pyBaba.ObjectType.IS: 'IS',
-                            pyBaba.ObjectType.YOU: 'YOU',
-                            pyBaba.ObjectType.FLAG: 'FLAG',
-                            pyBaba.ObjectType.WIN: 'WIN',
-                            pyBaba.ObjectType.WALL: 'WALL',
-                            pyBaba.ObjectType.STOP: 'STOP',
-                            pyBaba.ObjectType.ROCK: 'ROCK',
-                            pyBaba.ObjectType.PUSH: 'PUSH',
-                            pyBaba.ObjectType.WATER: 'WATER',
-                            pyBaba.ObjectType.SINK: 'SINK',
-                            pyBaba.ObjectType.LAVA: 'LAVA',
-                            pyBaba.ObjectType.MELT: 'MELT',
-                            pyBaba.ObjectType.HOT: 'HOT'}
-
-        for i in self.text_images:
-            self.text_images[i] = GIFImage(
-                './sprites/text/{}.gif'.format(self.text_images[i]))
-            self.text_images[i].scale(1.0)
+        pygame.sprite.Sprite.__init__(self)
 
 
 class ResultImage(pygame.sprite.Sprite):
