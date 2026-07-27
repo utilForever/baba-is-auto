a073afbf61c882362f51a10f138f8def16928fcd
Chris Ohk <utilforever@gmail.com>
2026-07-27 23:32:38 +0900

    test: avoid shadowing the map builtin

--- Tests/PythonTests/test_map.py
+++ Tests/PythonTests/test_map.py
@@ -10,10 +10,10 @@ import pyBaba
 
 
 def test_map_basic():
-    map = pyBaba.Map(5, 5)
-    map.AddObject(3, 4, pyBaba.ObjectType.BABA)
-    assert map.At(3, 3).HasType(pyBaba.ObjectType.ICON_EMPTY)
-    assert map.At(3, 4).HasType(pyBaba.ObjectType.BABA)
+    game_map = pyBaba.Map(5, 5)
+    game_map.AddObject(3, 4, pyBaba.ObjectType.BABA)
+    assert game_map.At(3, 3).HasType(pyBaba.ObjectType.ICON_EMPTY)
+    assert game_map.At(3, 4).HasType(pyBaba.ObjectType.BABA)
 
 
 def test_object_duplicate_stack():
