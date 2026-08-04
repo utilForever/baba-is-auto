78c93ad46e7c31de7f1cdafb2c9c6335800be9b4
Chris Ohk <utilforever@gmail.com>
2026-08-04 22:01:27 +0900

    build: simplify coverage configuration

--- CMake/CompileOptions.cmake
+++ CMake/CompileOptions.cmake
@@ -138,7 +138,7 @@ endif()
 
 # Code coverage - Debug only
 # NOTE: Code coverage results with an optimized (non-Debug) build may be misleading
-if (CMAKE_BUILD_TYPE MATCHES Debug AND (CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
+if (BUILD_COVERAGE AND CMAKE_BUILD_TYPE MATCHES Debug AND (CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
     set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
         -g
         -O0
